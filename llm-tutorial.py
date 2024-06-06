536061166d23d62b4db76f35ef61601e5738705e
andreeavodita <andreea.vodita11@gmail.com>
2024-06-06 19:04:02 +0300

    tried something new; cuda oom

--- llm-tutorial.py
+++ llm-tutorial.py
@@ -2,28 +2,46 @@ import torch
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, BitsAndBytesConfig
-from trl import SFTTrainer, SFTConfig
+from trl import SFTTrainer
 
 def train():
     train_dataset = load_dataset("tatsu-lab/alpaca", split="train")
 
-    quantization_config = BitsAndBytesConfig(llm_int8_enable_fp32_cpu_offload=True)
+    quantization_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        llm_int8_enable_fp32_cpu_offload=True
+    )
 
     tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
     tokenizer.pad_token = tokenizer.eos_token
+
     model = AutoModelForCausalLM.from_pretrained(
-        "Salesforce/xgen-7b-8k-base", torch_dtype=torch.float16, device_map="auto", quantization_config=quantization_config
+        "Salesforce/xgen-7b-8k-base",
+        torch_dtype=torch.float16,
+        device_map="auto",
+        offload_folder="./offload_folder",
     )
+    model = model.half()
 
     model.resize_token_embeddings(len(tokenizer))
     model = prepare_model_for_kbit_training(model)
-    peft_config = LoraConfig(r=16, lora_alpha=32, lora_dropout=0.05, bias="none", task_type="CAUSAL_LM")
+
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        task_type="CAUSAL_LM"
+    )
+
     model = get_peft_model(model, peft_config)
 
     training_args = TrainingArguments(
-        output_dir="xgen-7b-tuned-alpaca",
-        overwrite_output_dir=True,
-        per_device_train_batch_size=4,
+        output_dir="xgen-7b-tuned-alpaca-l1",
+        per_device_train_batch_size=1,
         optim="adamw_torch",
         logging_steps=100,
         learning_rate=2e-4,
@@ -35,20 +53,15 @@ def train():
         # push_to_hub=True,
     )
 
-    sft_config = SFTConfig(
-        output_dir="xgen-7b-tuned-alpaca",
-        dataset_text_field="text",
-    )
-
     trainer = SFTTrainer(
         model=model,
         train_dataset=train_dataset,
+        dataset_text_field="text",
+        max_seq_length=1024,
         tokenizer=tokenizer,
         args=training_args,
-        peft_config=peft_config,
         packing=True,
-        max_seq_length=1024,
-        # sft_config=sft_config
+        peft_config=peft_config,
     )
 
     trainer.train()
